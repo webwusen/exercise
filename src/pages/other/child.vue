02ef4db18c6eb82541f065f2beceb1bf536d2247
webwusen <898036454@qq.com>
2022-03-11 10:36:59 +0800

    修改

--- src/pages/other/child.vue
+++ src/pages/other/child.vue
@@ -1,28 +1,25 @@
+<script lang="ts">
+  import { toRefs } from 'vue'
+
+  export default {
+    setup(props: any) {
+      const { list } = toRefs(props)
+      return {
+        list
+      }
+    }
+  }
+</script>
+
 <template>
   <div>
     <ul>
-      <li v-for="(item, index) in newList" :key="index">{{ item }}</li>
+      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
     </ul>
     <teleport to="body">
       <div style="position: fixed; top: 0; right: 0">teleport</div>
     </teleport>
   </div>
 </template>
-
-<script lang="ts">
-  import { toRefs } from 'vue'
-  export default {
-    props: {
-      list: Array
-    },
-    setup(props) {
-      console.log(props)
-      const { list } = toRefs(props)
-      return {
-        newList: list
-      }
-    }
-  }
-</script>
 
 <style scoped></style>
